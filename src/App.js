6aeeefb3264706fbf1884d1b484126ca794a6ae4
007steve <stephen.plummer1.sp@gmail.com>
2020-12-28 15:02:43 -0500

    clean up app js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
 import "./App.css";
 import Nav from "./components/Nav";
 import SideBar from "./components/SideBar";
 import DataContainer from "./components/DataContainer";
+
 function App() {
   return (
     <div className="App">
       <Nav />
       <div className="container">
         <SideBar />
-        <DataContainer/>
+        <DataContainer />
       </div>
     </div>
   );
