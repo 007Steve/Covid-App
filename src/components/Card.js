39b1b12227105ca134ec7cab90c9658c8e16c596
007steve <stephen.plummer1.sp@gmail.com>
2020-12-28 15:06:35 -0500

    added data and up down arrow icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,38 @@
 import React from "react";
-import "../styles/Card.css";
+ import "../styles/Card.css";
 import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-function Card() {
+function Card({ confirmed, recovered, deaths }) {
   return (
     <div className="card shadow">
       <div className="card__cases">
+        <iframe
+          id="afe625bd-4172-4338-bcb5-b07a17fb9050"
+          src="https://www.vectary.com/viewer/v1/?model=afe625bd-4172-4338-bcb5-b07a17fb9050&env=studio3"
+          frameborder="0"
+          width="100%"
+          height="480"
+        ></iframe>
         <h3 className="card__casesTitle">Total Cases</h3>
         <div className="card__casesInfo">
-          <h3>12455</h3>
-          <ArrowDropUpIcon  className="green"/>
+          <h3>{confirmed}</h3>
+          <ArrowDropUpIcon className="green" />
         </div>
       </div>
       <div className="card__infoContainer">
         <div className="card__info">
           <p>Confirmed</p>
-          <span>1222</span>
+          <span>{confirmed}</span>
         </div>
 
         <div className="card__info">
           <p> Recovered</p>
-          <span>1222</span>
+          <span>{recovered}</span>
         </div>
 
         <div className="card__info">
           <p> Deaths</p>
-          <span>1222</span>
+          <span>{deaths}</span>
         </div>
       </div>
     </div>
