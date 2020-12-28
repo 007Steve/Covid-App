9d6d94b28e7318c356c91e2c9dfd2a38c221f9b7
007steve <stephen.plummer1.sp@gmail.com>
2020-12-28 15:09:49 -0500

    added data and up down arrow icons

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import "../styles/CountryCard.css"
+import React from "react";
+import "../styles/CountryCard.css";
 import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-function CountryCard({name,oldCase,newCase}) {
-    return (
-        <div className="countrycard ">
-           
-                <p>{name}</p>
-                <span>{oldCase}</span>
-                <p>{newCase}</p>
-                <ArrowDropDownIcon className="red"/>
-           
-        </div>
-    )
+function CountryCard({ name, oldCase, newCase }) {
+    const up = newCase 
+    console.log(up)
+  return (
+    <div className="countrycard ">
+      <p>{name}</p>
+      <span></span>
+      <p></p>
+      {up?  <ArrowDropUpIcon className="green" /> : <ArrowDropDownIcon className="red" />}
+     
+    </div>
+  );
 }
 
-export default CountryCard
+export default CountryCard;
