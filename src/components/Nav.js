81b63c1d0f3563430d74498b7625a1aacb3adeef
007steve <stephen.plummer1.sp@gmail.com>
2020-12-28 15:11:18 -0500

    added  search input

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Nav.css";
 import SearchIcon from "@material-ui/icons/Search";
 import TuneIcon from "@material-ui/icons/Tune";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import { useDispatch } from "react-redux";
+import { addInput } from "../features/inputSlice";
+
 function Nav() {
+  const [input, setInput] = useState([]);
+  const dispatch = useDispatch();
+
   return (
     <div className="nav">
-      <img src="https://www.pngkey.com/png/full/171-1713121_transparent-psd-3d-blue-seapunk-neon-net-art.png" alt="" />
-
-      <div className="center">
-        <input type="text" />
-        <SearchIcon />
+      <div className="nav__left">
+        <img
+          src="https://www.pngkey.com/png/full/171-1713121_transparent-psd-3d-blue-seapunk-neon-net-art.png"
+          alt=""
+        />
+        <h3>Covid Cases</h3>
       </div>
-
-      <div className="right">
-        <PermIdentityIcon className="icons"/>
-        <TuneIcon className="icons" />
+      <form className="nav__center" type="submit">
+        <input
+          type="text"
+          placeholder="Search "
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <SearchIcon type="submit"
+          onClick={() => {
+            dispatch(addInput(input));
+            setInput("");
+          }}
+        />
+      </form>
+      <div className="nav__right">
+        <PermIdentityIcon className="nav__icons" />
+        <TuneIcon className="nav__icons" />
       </div>
     </div>
   );
