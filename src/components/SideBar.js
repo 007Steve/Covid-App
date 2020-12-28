5f466fe43a173f1a18dde4bcce7183f2fb31d6df
007steve <stephen.plummer1.sp@gmail.com>
2020-12-28 15:12:33 -0500

    added fetch data

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/SideBar.css";
 import Card from "../components/Card";
 import CountryCard from "./CountryCard";
 function SideBar() {
+  const [global, setGlobal] = useState([]);
+  const [countries, setCountries] = useState([]);
+
+  // fetch the global data stats
+  useEffect(() => {
+    async function fetchData() {
+      fetch(`https://disease.sh/v3/covid-19/all`)
+        .then((response) => response.json())
+        .then((data) => setGlobal(data));
+    }
+
+    fetchData();
+  }, []);
+  // fetch the countries list
+  useEffect(() => {
+    async function countriesData() {
+      fetch(`https://disease.sh/v3/covid-19/countries`)
+        .then((response) => response.json())
+        .then((data) => setCountries(data));
+    }
+
+    countriesData();
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__container">
         <h2> Global Statistics</h2>
-        <Card />
-        
+        <Card
+          confirmed={global.cases}
+          recovered={global.recovered}
+          deaths={global.deaths}
+        />
+        {setCountries ? setCountries : "loading"}
+
         <div className="sidebar__countries shadow">
-          <CountryCard name="USA" oldCase="1233" newCase="2332" />
-          <CountryCard name="USA" oldCase="1233" newCase="2332" />
-          <CountryCard name="USA" oldCase="1233" newCase="2332" />
-          <CountryCard name="USA" oldCase="1233" newCase="2332" />
-          <CountryCard name="USA" oldCase="1233" newCase="2332" />
-          <CountryCard name="USA" oldCase="1233" newCase="2332" />
-          <CountryCard name="USA" oldCase="1233" newCase="2332" />
+          {countries.map((data) => (
+            <CountryCard
+              name={data.country}
+              oldCase={data.cases}
+              newCase={data.todayCases}
+            />
+          ))}
         </div>
       </div>
     </div>
