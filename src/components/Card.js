9ecd6b265bfcebb011d9fdef61bbe332a436a867
007steve <stephen.plummer1.sp@gmail.com>
2020-12-28 15:27:48 -0500

    updated card remove icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
  import "../styles/Card.css";
 import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 function Card({ confirmed, recovered, deaths }) {
   return (
     <div className="card shadow">
       <div className="card__cases">
         <iframe
+        title="3d"
           id="afe625bd-4172-4338-bcb5-b07a17fb9050"
           src="https://www.vectary.com/viewer/v1/?model=afe625bd-4172-4338-bcb5-b07a17fb9050&env=studio3"
           frameborder="0"
